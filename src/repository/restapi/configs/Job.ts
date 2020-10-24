db702bc14354ad490f216f8ce89149f5daaecd39
lucasdias2906 <lucasdias2906@gmail.com>
2020-10-24 18:08:40 -0300

    comit atualozando a controller

---
+++ src/repository/restapi/configs/Job.ts
@@ -0,0 +1,16 @@
+import { schedule } from 'node-cron'
+
+
+class Jobs {
+
+    public job(temp: Number, func: Function) {
+        return schedule(`*/${temp} * * * *`, async () => {
+            console.log("ENTROU NO JOB CONFIG")
+            return await func()
+        })
+    }
+}
+
+const JobsSchedule = new Jobs()
+
+export default JobsSchedule
--- src/repository/restapi/controllers/DevController.ts
+++ src/repository/restapi/controllers/DevController.ts
@@ -1,22 +1,25 @@
 
 import { Request, Response } from 'express';
 import axios from 'axios';
-import { schedule } from 'node-cron'
+import JobConfig from '../configs/Job'
 
 
 import User from '../../models/DevSchema'
 
 const key = "AIzaSyCi40KsvfAENVTteNcHVACNRKzAFUhbAxI"
 
-
 class DevController {
 
     static async update(req: Request, res: Response) {
 
-        const users: any = await User.find()
+        console.log("Entrou no UPDATE-CONTROLLER")
 
-        schedule("*/1 * * * *", async () => {
-
+        
+        
+        JobConfig.job(1, async () => {
+            const users: any = await User.find()
+            console.log("USERSS", users)
+            
             console.log("Só será executado em um minuto e repetirá (de 1 em 1 minutos) até ser desativado...");
 
             await new Promise(resolve => {
@@ -25,6 +28,10 @@ class DevController {
 
                     
                     if (!user.lat || !user.lng) {
+
+                        console.log("Entroou na validacao do IF")
+
+                        console.log("USER 1", user)
 
                         const addressUrl = encodeURIComponent(user.address[0].street)
                         
@@ -39,6 +46,8 @@ class DevController {
                         const objteste = await { ...{ _id: user.id }, ...location, payload: { ...apiRes.data?.results[0] } }
 
                         await User.findOneAndUpdate({ _id: user.id }, objteste)
+
+                        console.log("USER 2", user)
 
                         if (index === users.length) {
                             resolve(true)
